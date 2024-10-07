ef3827c6ed01729678721a2292357180936e001a
Kinyock <terrykinyock6@gmail.com>
2024-10-07 19:38:03 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-//base by DGXeon
+//base by Killer Ghost ☠️ 💀 🚬 
 //re-upload? recode? copy code? give credit ya :)
 //YouTube: @GlobalTechInfo
 //Telegram: https://t.me/GlobalTechInc
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
+global.ownernomer = "237673804138"
 global.ownername = "Qasim Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
+global.ownernumber = '237673804138'  //creator number
 global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.botname = 'killer Ghost ☠️💀🚬' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: +237673804138'
 
 //console view/theme
 global.themeemoji = '🪀'
